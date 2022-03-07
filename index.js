6216f3bf3f47e8b2095b42e22964a070d3ca35db
akukamil <chamilioner_kid@mail.ru>
2022-03-07 21:45:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4370,8 +4370,8 @@ async function load_resources() {
 	return;*/
 
 
-	//let git_src="https://akukamil.github.io/quoridor"
-	let git_src=""
+	let git_src="https://akukamil.github.io/quoridor"
+	//let git_src=""
 
 
 	game_res=new PIXI.Loader();
