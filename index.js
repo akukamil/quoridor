28a84a51eccb5ed15c0f89567dd7fa8c1b96e5f1
akukamil <chamilioner_kid@mail.ru>
2024-02-07 15:23:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5378,6 +5378,7 @@ auth2={
 		return '';
 		
 	}
+
 }
 
 function resize() {
@@ -5573,7 +5574,7 @@ async function init_game_env(lng) {
 		my_data.name=`${my_data.name} (${my_data.country})`
 	
 	//загружаем мои данные в кэш
-	await players_cache.update(my_data.uid,{pic_url:my_data.pic_url,country:my_data.country});
+	await players_cache.update(my_data.uid,{pic_url:my_data.pic_url,country:my_data.country,name:my_data.name,rating:my_data.rating});
 	await players_cache.update_avatar(my_data.uid);
 	
 	//устанавливаем фотки в попап
